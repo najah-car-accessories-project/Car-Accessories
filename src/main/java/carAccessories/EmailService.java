58edc0cd52b52ce0cc8cce4dd04404f0cfd40bc5
Mohammad Badawi <>
2023-12-18 19:37:49 +0200

    refactoring code

--- src/main/java/carAccessories/EmailService.java
+++ src/main/java/carAccessories/EmailService.java
@@ -55,7 +55,7 @@ public class EmailService {
     	            
     	        }
     	        catch (MessagingException m){
-    	             m.printStackTrace();
+    	             
     	        }
     	        
     }
